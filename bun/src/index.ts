20735b7a284464009010e6e9a4deb77ae25ad5dc
Mateo Fernandez <mateo@nudibranches.tech>
2025-05-16 11:57:27 +0200

    feat: add examples for postgres

--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,13 +1,24 @@
 import type { ClientError } from "openid-client";
-import { authorizationCodeFlow } from "./auth_utils/authorization_code";
 import { doHttpRequest } from "./requests/http";
+import { clientCredentialsFlow } from "./auth_utils/client_credentials";
+import { doBunPostgresRequest, doPostgresJsRequest } from "./requests/postgres";
 
 // Retrieve access_token
-const tokens = await authorizationCodeFlow().catch((e: ClientError) => {
+const tokens = await clientCredentialsFlow().catch((e: ClientError) => {
   console.error(e);
   process.exit(1);
 });
 
+// Postgres Examples
+console.log("\nRequest with Bun postgres driver:");
+const bunPgResponse = await doBunPostgresRequest(tokens.access_token);
+console.log(bunPgResponse);
+
+console.log("\nRequest with Postgres.js driver:");
+const pgJsResponse = await doPostgresJsRequest(tokens.access_token);
+console.log(pgJsResponse);
+
 // HTTP Example
 console.log("\nRequest with HTTP:");
-await doHttpRequest(tokens.access_token);
+const httpResponse = await doHttpRequest(tokens.access_token);
+console.log(httpResponse);
---
+++ bun/src/requests/postgres.ts
@@ -0,0 +1,95 @@
+import { SQL } from "bun";
+import postgres from "postgres";
+
+export async function doBunPostgresRequest(accessToken: string) {
+  const config = fillConfig();
+
+  const sql = await new SQL({
+    adapter: "postgres",
+    hostname: config.hostname.value,
+    port: config.port.value,
+    database: config.database.value,
+    password: accessToken,
+  })
+    .connect()
+    .catch((e) => console.error(e));
+  if (!sql) process.exit(1);
+
+  const result = await sql.unsafe(config.query.value).simple();
+  sql.end(); // closing connection manually
+
+  return result;
+}
+
+export async function doPostgresJsRequest(accessToken: string) {
+  const config = fillConfig();
+
+  const sql = postgres({
+    hostname: config.hostname.value,
+    port: parseInt(config.port.value),
+    database: config.database.value,
+    password: accessToken,
+    fetch_types: false,
+  });
+
+  const result = await sql.unsafe(config.query.value).simple();
+  sql.end(); // closing connection manually
+
+  return result;
+}
+
+/// Some utils, don't worry
+type Config = {
+  hostname: Prompt;
+  port: Prompt;
+  database: Prompt;
+  query: Prompt;
+};
+
+type Prompt = {
+  label: string;
+  help?: string;
+  value: string;
+};
+
+function fillConfig(): Config {
+  const config: Config = {
+    hostname: {
+      label: "Enter a hostname:",
+      value: "",
+    },
+    port: {
+      label: "Enter a port:",
+      value: "",
+    },
+    database: {
+      label: "Enter your DataDock ID (= Database):",
+      value: "",
+    },
+    query: {
+      label: "Enter your SQL Query:",
+      help: "Example: SELECT * FROM {catalog}.{schema}.{table} LIMIT 10",
+      value: "",
+    },
+  };
+
+  for (const configEntry of Object.values(config)) {
+    let value = null;
+    if (configEntry.help) {
+      console.log(`> ${configEntry.label}`);
+      console.log(`> ${configEntry.help}`);
+      value = prompt(">");
+    } else {
+      value = prompt(`> ${configEntry.label}`);
+    }
+
+    if (value === null) {
+      console.error(`No value provided`);
+      process.exit(1);
+    }
+
+    configEntry.value = value;
+  }
+
+  return config;
+}
